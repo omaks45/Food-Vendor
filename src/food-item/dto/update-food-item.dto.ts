e31196253fe45986971b317cfabd471c1b007743
omaks45 <danielgozie5@gmail..com>
2026-02-14 15:15:01 +0100

    git hub updated

--- src/food-item/dto/update-food-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateFoodItemDto } from './create-food-item.dto';
-
-export class UpdateFoodItemDto extends PartialType(CreateFoodItemDto) {}
