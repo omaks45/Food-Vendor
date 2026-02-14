cca6318bccde4b2154d3a55cf160bba6716e08ba
omaks45 <danielgozie5@gmail..com>
2026-02-14 16:44:37 +0100

    current user decorator updated

---
+++ src/common/decorators/current-user.decorator.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUser = createParamDecorator(
+    (data: string, ctx: ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest();
+        const user = request.user;
+
+        return data ? user?.[data] : user;
+    },
+);
